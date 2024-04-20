c5d7372751807792c44ade48fad87e35eb33c827
Sn-121 <65838620+Sn-121@users.noreply.github.com>
2024-04-20 09:38:48 +0800

    Update psnrssim.py

--- psnrssim.py
+++ psnrssim.py
@@ -11,16 +11,15 @@ warnings.filterwarnings('ignore')
 
 SSIM_list = []
 PSNR_LIST = []
-# clear_img_path = "D:/BaiduNetdiskDownload/Huawei_Nikon/hua/test/Result"  # 清晰图像文件夹路径
-# hazy_img_path = "D:/BaiduNetdiskDownload/Huawei_Nikon/hua/test/high" # 待去雾图像文件夹路径
-clear_img_path = "D:/Github-Code/MainNet/LOL/high"  # 清晰图像文件夹路径
-hazy_img_path = "D:/Github-Code/MainNet/LOL/low"# 待去雾图像文件夹路径
-# clear_img_path = "result/"  # 清晰图像文件夹路径
-# hazy_img_path = "test/target/"  # 待去雾图像文件夹路径
-
-clear_img_names = os.listdir(clear_img_path) # 获取所有的清晰图像文件名
+# clear_img_path = "D:/BaiduNetdiskDownload/Huawei_Nikon/hua/test/Result"  
+# hazy_img_path = "D:/BaiduNetdiskDownload/Huawei_Nikon/hua/test/high" 
+clear_img_path = "D:/Github-Code/MainNet/LOL/high"  
+hazy_img_path = "D:/Github-Code/MainNet/LOL/low"
+# clear_img_path = "result/"  
+# hazy_img_path = "test/target/"  
+clear_img_names = os.listdir(clear_img_path) 
 hazy_img_names = []
-# 遍历清晰图像文件名列表，找到对应的待去雾图像文件名
+
 for name in clear_img_names:
     hazy_img_names.append(name[:] )
 
@@ -29,17 +28,14 @@ for i in range(len(clear_img_names)):
     hazy_img = cv2.imread(os.path.join(hazy_img_path, hazy_img_names[i]))
     if clear_img.shape[0] != hazy_img.shape[0] or clear_img.shape[1] != hazy_img.shape[1]:
         pil_img = Image.fromarray(hazy_img)
-        pil_img = pil_img.resize((clear_img.shape[1], clear_img.shape[0])) # 和clear_img的宽和高保持一致
+        pil_img = pil_img.resize((clear_img.shape[1], clear_img.shape[0])) 
         hazy_img = np.array(pil_img)
 
-    # 计算PSNR
-    # PSNR越大，代表着图像质量越好。
+    
     PSNR = peak_signal_noise_ratio(clear_img, hazy_img)
     print(i+1, 'PSNR: ', PSNR)
     PSNR_LIST.append(PSNR)
 
-
-    # 计算SSIM
     SSIM = structural_similarity(clear_img, hazy_img, multichannel=True)
     print(i+1, 'SSIM: ', SSIM)
     SSIM_list.append(SSIM)
